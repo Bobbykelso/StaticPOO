da6778a17bbde0c20dac6c44d68a3bb0d53422b1
Bobbykelso <barth.aurelien182@gmail.com>
2021-05-20 23:32:10 +0200

    Speedometer conversion

--- Speedometer.php
+++ Speedometer.php
@@ -5,11 +5,11 @@ class Speedometer
     public const KM_CONVERSION = 0.621;
     public const MILES_CONVERSION = 1.609;
 
-    public static function convertKmToMiles(int $distance): float
+    public static function convertKmToMiles(int $distance)
     {   
         return $distance * self::KM_CONVERSION;
     }
-    public static function convertMilesToKm(int $distance): float
+    public static function convertMilesToKm(int $distance)
     {   
         return $distance * self::MILES_CONVERSION;
     }
